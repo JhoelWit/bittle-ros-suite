f0df4d427eea4385866ed210dfe59d80264ea303
Jhoel <jhoelwit@buffalo.edu>
2023-03-07 17:54:13 -0500

    filled in header and cpp files for bittle driver

--- include/bittle_driver/bittle_driver.h
+++ include/bittle_driver/bittle_driver.h
@@ -1,24 +1,45 @@
 #ifndef BITTLE_DRIVER
 #define BITTLE_DRIVER
 
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <serial/serial.h>
+#include <unordered_map>
+
 
 class BittleDriver{
-    private:
-    float turn_radius_;
-    float linear_vel_;
+private:
+    int bittleState = 0;
+    float turnRadius_;
+    float linearVel_;
+    serial::Serial bittleSerial;
+    std::unordered_map<int, std::string> directions = 
+    {
+        {1, "kwk"},     //  walk
+        {-1, "kbk"},    // move backwards
+        {2, "kcrR"},    // crawl right
+        {3, "kcrL"},    // crawl left
+        {0, "kbalance"} // imu balancing
+    };
 
-    public:
+public:
     // Constructor
-    BittleDriver();
+    BittleDriver(std::string port="/dev/ttyS0", int baudRate=115200);
 
     // Destructor
     ~BittleDriver();
 
     // Set linear speed of the robot
-    void set_speed(float speed);
+    void setSpeed(float speed);
 
     // Set turn radius of the robot
-    void set_turn_radius(float turn_radius);
+    void setTurnRadius(float turn_radius);
+
+    // We'll add a callback for the ROS wrapper
+    void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg);
+
+    // Serial methods
+    void serialWrite(std::string token);
 
 };
 
--- src/bittle_driver.cpp
+++ src/bittle_driver.cpp
@@ -1,20 +1,50 @@
 #include <bittle_driver/bittle_driver.h>
-#include <iostream> // Used for cout or cin
+#include <iostream>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <string>
+#include <serial/serial.h>
 
 
 // Constructor
-BittleDriver::BittleDriver(){
+BittleDriver::BittleDriver(std::string port, int baudRate){
     // Constructor implementation
+    // For now we'll initialize the directions and other important serial information
+    // std::string port = "/dev/ttyS0";
+    // int baudRate = 115200; // Can also be 57600, depends on arduino equipment
+    // Initialize the serial object
+    serial::Serial bittle_serial(port, baudRate, serial::Timeout::simpleTimeout(1000));
 };
 
 BittleDriver::~BittleDriver(){
     // Destructor implementation
+    bittleSerial.close();
 }
 
-void BittleDriver::set_speed(float speed){
-    linear_vel_ = speed;
+void BittleDriver::setSpeed(float speed){
+    linearVel_ = speed;
 }
 
-void BittleDriver::set_turn_radius(float radius){
-    turn_radius_ = radius;
+void BittleDriver::setTurnRadius(float radius){
+    turnRadius_ = radius;
+}
+
+void BittleDriver::cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg){
+    ROS_INFO_STREAM("Received a twist message! Linear: " << msg->linear.x << "Angular: " << msg->angular.z);
+    int msgState;
+    if (msg->linear.x > 0) {msgState = 1;}
+    else if (msg->linear.x < 0) {msgState = -1;}
+    else if (msg->angular.z > 0) {msgState = 2;}
+    else if (msg->angular.z < 0) {msgState = 3;}
+    else {msgState = 0;}
+
+    if (msgState != bittleState)
+    {
+        serialWrite(directions[msgState]);
+        bittleState = msgState;
+    }
+}
+
+void BittleDriver::serialWrite(std::string token){
+    bittleSerial.write(token.c_str());
 }
