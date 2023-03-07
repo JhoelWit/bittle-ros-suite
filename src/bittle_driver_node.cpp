8cd90949553037f18cd0d615139cf12de981c2e0
Jhoel <jhoelwit@buffalo.edu>
2023-03-07 17:56:01 -0500

    Added ros wrapper for the driver, and corresponding launch files for host machine and remote machine

---
+++ src/bittle_driver_node.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <bittle_driver/bittle_driver.h>
+
+int main(int argc, char **argv)
+{
+    // Initialize ROS node
+    ros::init(argc, argv, "bittle_driver");
+    ros::NodeHandle nh("~");
+
+    // Get serial port and baudrate from ROS parameters
+    std::string port;
+    int baudRate;
+    nh.param<std::string>("port", port, "/dev/ttyS0");
+    nh.param<int>("baudrate", baudRate, 115200);
+
+    // Instantiate BittleDriver object
+    BittleDriver driver(port, baudRate);
+
+    // Subscribe to cmd_vel topic
+    ros::Subscriber sub = nh.subscribe("cmd_vel", 1, &BittleDriver::cmdVelCallback, &driver);
+
+    // Spin
+    ros::spin();
+
+    return 0;
+}
